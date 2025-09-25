45b188e1f401336e69ae7b48c144c443340fea53
utibe solomon <utibesolomon6@gmail.com>
2025-09-25 19:42:51 +0100

    Added Vercel engine

--- prisma/migrations/20250714120941_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "status" TEXT NOT NULL DEFAULT 'OPEN'
-);
